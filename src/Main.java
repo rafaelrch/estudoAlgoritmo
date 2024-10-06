401ce6bc33105ebdc5b32a2e761e9492ea9e8eb3
Rafael Rocha <rafaelrocha250304@gmail.com>
2024-10-06 19:21:43 -0300

    example 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        //Example 1
+
         String produto1 = "Computador";
         String produto2 = "TV";
         double preco1 = 2100.5;
@@ -11,13 +14,27 @@ public class Main {
         String genero = "F";
 
         System.out.println("-----------------------------------------");
-        System.out.println("Produtos:");
+        System.out.println("Produtc:");
         System.out.println("O produto " + produto1 + " custa R$ " + preco1);
         System.out.println("O produto " + produto2 +  " custa R$ " + preco2);
         System.out.println(" ");
         System.out.println("Código = " + codigo);
         System.out.println(" ");
         System.out.println("Dados da pessoa: gênero " + genero + " e idade " + idade);
-        System.out.println("-----------------------------------------");
+        System.out.println("--------------------------------------------------");
+
+
+        // Example 2
+       double b, B, h, area;
+
+       b = 6.0;
+       B = 8.0;
+       h = 5.0;
+
+       area = (b + B) / 2.0 * h;
+
+       System.out.println("The area of this example is " + area);
+
+
     }
 }
