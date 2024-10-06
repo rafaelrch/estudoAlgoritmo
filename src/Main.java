446e4a91d8a86df2080b9582b2d99d622bdcea7b
Rafael Rocha <rafaelrocha250304@gmail.com>
2024-10-06 19:01:54 -0300

    example 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String produto1 = "Computador";
+        String produto2 = "TV";
+        double preco1 = 2100.5;
+        double preco2 = 1830.0;
+        int idade = 30;
+        int codigo = 5291;
+        String genero = "F";
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Produtos:");
+        System.out.println("O produto " + produto1 + " custa R$ " + preco1);
+        System.out.println("O produto " + produto2 +  " custa R$ " + preco2);
+        System.out.println(" ");
+        System.out.println("Código = " + codigo);
+        System.out.println(" ");
+        System.out.println("Dados da pessoa: gênero " + genero + " e idade " + idade);
+        System.out.println("-----------------------------------------");
+    }
+}
